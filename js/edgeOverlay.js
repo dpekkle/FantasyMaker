9c909cb43972d96db29cf11cb1eca6294475bb78
dazMurph <dazmurph90@gmail.com>
2016-05-31 18:27:05 +1000

    Fixed add cond/outcome
    Add condition and outcomes now works with materialize. Still need to fix
    modal window collapsibles and implement delete.

--- js/edgeOverlay.js
+++ js/edgeOverlay.js
@@ -8,8 +8,8 @@ var edgeOverlay_selectedEdge; // edge that the user is modifying
 
 //TODO - Get this working with materialize
 
-
 function populateEdgeOverlay(edge){
+	
 	
 	//reset for new page overlay
 	edgeOverlay_conditionCount = 0; 
@@ -28,6 +28,10 @@ function populateEdgeOverlay(edge){
 		$('#condition_compList' + i).val(curr.comparison);
 		$('#condition_value' + i).val(curr.value);
 		
+		//refresh drop down boxes
+		$('#condition_statTypeList' + i).material_select();
+		$('#condition_compList' + i).material_select();
+		
 	}
 		
 	
@@ -42,20 +46,23 @@ function populateEdgeOverlay(edge){
 		$('#outcome_statTypeList' + i).val(curr.stat);
 		$('#outcome_modList' + i).val(curr.modifier);
 		$('#outcome_value' + i).val(curr.value);
+		
+		//refresh drop down boxes
+		$('#outcome_statTypeList' + i).material_select();
+		$('#outcome_modList' + i).material_select();
 	}
 		
 }
 
 function saveEdge(){
 	
-	//for all condition in ui
+	//for all conditions in ui
 	for(var i = 0; i<edgeOverlay_conditionCount; i++){
 		
 		//get values of menus/text from ui
 		var stat = $('#condition_statTypeList' + i).val();
 		var comp = $('#condition_compList' + i).val();
 		var val = $('#condition_value' + i).val();
-		
 		
 		//convert ui condition row into json obj
 		var newConst = conditions_createCondition(stat,comp,val); 
@@ -121,8 +128,11 @@ function populateStatsDropDownMenu(index, type){
 	 //for all defined stat types
 	for(var i = 0; i<project_project.statTypes.length; i++){
 		//append element to drop down menu
-		 $(id).append( '<option value="' + project_project.statTypes[i].type + '">' + project_project.statTypes[i].type + "</option>");
+		$(id).append( '<option value="' + project_project.statTypes[i].type + '">' + project_project.statTypes[i].type + "</option>");
+		$(id).material_select();
 	}
+	
+	
 	
 } 
 
@@ -132,10 +142,12 @@ function edgeOverlay_addCondition(){
 
 	//TODO - this is still using Bootstrap - fix
 	//html of condition row
-	var conditionHtml = '<li><ul class="nav nav-pills" id="conditionsList"><li><select class="form-control" id="condition_statTypeList' + edgeOverlay_conditionCount +'"><select></li><li><select class="form-control" id="condition_compList' + edgeOverlay_conditionCount +'"><option value="=">=</option><option value=">">></option><option value="<"><</option><option value=">=">>=</option><option value="<="><=</option></select></li><li><input type="number" class="form-control" id="condition_value' + edgeOverlay_conditionCount +'" placeholder="Enter value" ></li></ul><li>';
+	var conditionHtml = '<li><div class="input-field col s12" style = "display: inline-block; width: 20%; padding-left: 3%"><select id="condition_statTypeList' + edgeOverlay_conditionCount +'"></select></div><div class="input-field col s12" style = "display: inline-block; width: 20%; padding-left: 1%; padding-right: 1%;"><select id="condition_compList' + edgeOverlay_conditionCount +'"><option value=">">></option><option value="<"><</option><option value=">=">>=</option><option value="<="><=</option><option value="=">=</option></select></div><input placeholder="Enter Value" id="condition_value' + edgeOverlay_conditionCount +'" type="number" class="validate" style = "display: inline-block; width: 20%;"><div id="DummyDiv" class="input-field col s12" style = "display: inline-block; width: 30%;"></div><a id="condition_delete' + edgeOverlay_conditionCount +'" class="btn-floating btn waves-effect waves-light red" onclick=removeCondition(' + edgeOverlay_conditionCount + ')><i class="material-icons">delete</i></a></li>';
 	
 	//add html to conditionList html element
 	$('#conditionsList').append(conditionHtml);
+	$('#condition_statTypeList' + edgeOverlay_conditionCount).material_select();
+	$('#condition_compList' + edgeOverlay_conditionCount).material_select();
 	populateStatsDropDownMenu(edgeOverlay_conditionCount,"CONDITION"); //populate the statType menu
 
 	edgeOverlay_conditionCount++;
@@ -145,12 +157,21 @@ function edgeOverlay_addOutcome(){
 
 	//TODO - this is still using Bootstrap - fix
 	//html of outcome row
-	var outcomeHtml = '<li><ul class="nav nav-pills"><li><select class="form-control" id="outcome_statTypeList' + edgeOverlay_outcomeCount + '"><select></li><li><select class="form-control" id="outcome_modList' + edgeOverlay_outcomeCount +'"><option value="+">+</option><option value="-">-</option><option value="/">/</option><option value="*">*</option><option value="=">=</option></select></li><li><input type="number" class="form-control" id="outcome_value' + edgeOverlay_outcomeCount +'" placeholder="Enter value"></li></ul></li>';
-	
+	var outcomeHtml = '<li><div class="input-field col s12" style = "display: inline-block; width: 20%; padding-left: 3%"><select id="outcome_statTypeList' + edgeOverlay_outcomeCount +'"></select></div><div class="input-field col s12" style = "display: inline-block; width: 20%; padding-left: 1%; padding-right: 1%;"><select id="outcome_modList' + edgeOverlay_outcomeCount +'"><option value="+">+</option><option value="-">-</option><option value="*">*</option><option value="/">/</option><option value="=">=</option></select></div><input placeholder="Enter Value" id="outcome_value' + edgeOverlay_outcomeCount +'" type="number" class="validate" style = "display: inline-block; width: 20%;"><div id="DummyDiv" class="input-field col s12" style = "display: inline-block; width: 30%;"></div><a id="outcome_delete' + edgeOverlay_outcomeCount +'" class="btn-floating btn waves-effect waves-light red" onclick=removeOutcome('+ edgeOverlay_outcomeCount +')><i class="material-icons">delete</i></a></li>';	
 	//add html to outcomeList
 	$('#outcomeList').append(outcomeHtml);
+	$('#outcome_statTypeList' + edgeOverlay_outcomeCount).material_select();
+	$('#outcome_modList' + edgeOverlay_outcomeCount).material_select();
 	populateStatsDropDownMenu(edgeOverlay_outcomeCount,"OUTCOME"); //populate the statType menu
 	edgeOverlay_outcomeCount++;
+}
+
+function removeCondition(id){
+	console.log("Removing condition " + id);
+}
+
+function removeOutcome(id){
+	console.log("Removing outcome " + id);
 }
 
 					
