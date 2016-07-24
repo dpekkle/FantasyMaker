c44f4447407dd5e46678cda2753f3cd41fda7359
dpekkle <danielle.cerisier@gmail.com>
2016-07-24 23:01:17 +1000

    Grid snapping (for page style overlay)

--- js/dragDrop.js
+++ js/dragDrop.js
@@ -1,43 +1,80 @@
-// target elements with the "draggable" class
-interact('.drag-element:not([locked])')
-	.draggable({
-		// enable inertial throwing
-		inertia: true,
-		// keep the element within the area of it's parent
-		restrict: {
-		  restriction: "parent",
-		  endOnly: false,
-		  elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-		},
-		// enable autoScroll
-		autoScroll: true,
+goog.provide('dragDrop')
 
-		// call this function on every dragmove event
-		onmove: dragMoveListener,
-		// call this function on every dragend event
-		onend: function (event) {
-		  console.log(event);
-		  var textEl = event.target.querySelector('p');
+grid_snapping_mode = false;
 
-		  textEl && (textEl.textContent =
-			'moved a distance of '
-			+ (Math.sqrt(event.dx * event.dx +
-						 event.dy * event.dy)|0) + 'px');
-		}
-	})
-	.allowFrom('.handle')
+//create a grid pattern
+var grid_targets = interact.createSnapGrid({
+  x: 10, //grid spacing between x points
+  y: 10, 
+  range: 10, //range from grid point from which to snap
+  offset: {x:-2, y:2}
+});
+
+function initSnapOptions()
+{
+	if (grid_snapping_mode)
+	{
+		return {
+			targets: [grid_targets],
+			relativePoints: [{x:1, y:1}], //upper left corner binds to grid
+			endOnly: false	
+		};
+	}
+	else
+		return {targets: [{x:0, y:0, range: 0}]};
+}
+
+//load grid into options
+var snap_options = initSnapOptions();
+
+function setInteractions()
+{
+	// target elements with the "draggable" class
+	interact('.drag-element:not([locked])')
+		.draggable({
+			snap: snap_options,
+			// disable inertial throwing
+			inertia: false,
+			// keep the element within the area of it's parent
+			restrict: {
+			  restriction: "parent",
+			  endOnly: false,
+			  elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+			},
+			// enable autoScroll
+			autoScroll: true,
+
+			// call this function on every dragmove event
+			onmove: dragMoveListener,
+			// call this function on every dragend event
+			onend: function (event) {
+			  console.log(event);
+			}
+		})
+		.allowFrom('.handle');
 
 
-interact('.resize-element')
-	.resizable({
-		edges: { left: false, right: true, bottom: true, top: false}
-	})
-	.on('resizemove', function (event) {
-		var tar = event.target;
-		// update the element's dimensions
-		tar.style.width  = checkBounds(tar.parentNode.getAttribute('data-x'), event.rect.width, $('#pagecontainers').width()) + 'px';
-		tar.style.height = checkBounds(tar.parentNode.getAttribute('data-y'), event.rect.height, $('#pagecontainers').height() - $('.handle').height()) + 'px';
-	});
+	interact('.resize-element')
+		.resizable({
+			snap: snap_options,
+			edges: { left: false, right: true, bottom: true, top: false}
+		})
+		.on('resizemove', function (event) {
+			var tar = event.target;
+			// update the element's dimensions
+			tar.style.width  = checkBounds(tar.parentNode.getAttribute('data-x'), event.rect.width, $('#pagecontainers').width()) + 'px';
+			tar.style.height = checkBounds(tar.parentNode.getAttribute('data-y'), event.rect.height, $('#pagecontainers').height() - $('.handle').height()) + 'px';
+		});
+}
+
+setInteractions();
+
+function toggleGridMode()
+{
+	grid_snapping_mode = !grid_snapping_mode;
+	snap_options = initSnapOptions();
+	setInteractions();	
+}
 
 function checkBounds(offset, dimension, limit)
 {
@@ -85,6 +122,7 @@ function modalBounds(){
 	if ($('#page-modal').hasClass("open"))
 	{
 		$('.resize-element').each(function(){
+			console.log("Test resize");
 			//recalculate bounds
 			var tar = $(this);
 			$(this).width(checkBounds(tar.parent().attr('data-x'), tar.width(), $('#pagecontainers').width()));
