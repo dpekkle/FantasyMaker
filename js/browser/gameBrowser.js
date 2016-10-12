867cff4637cc82b6ff6853edfc20f868be4af2a2
Russell <rhowell1110@gmail.com>
2016-10-12 20:31:44 +1100

    Game browser update

--- js/browser/gameBrowser.js
+++ js/browser/gameBrowser.js
@@ -22,42 +22,64 @@ goog.require('browser_httpRequests')
 
 */
 
-var gameBrowser_allProjects //obj that will hold all users and their project names
-gameBrowser_loadUsers()//calling on load just for demo purposes, not sure if thats what you want.
+var gameBrowser_allProjects; //obj that will hold all users and their project names
+gameBrowser_loadUsers();//calling on load just for demo purposes, not sure if thats what you want.
 
 function gameBrowser_loadUsers(){
-  //pass by reference in js only works on contents of objs
-  var res = {
-    "data" : {} //stores the results of the http request temporarily
-  }
-  $.when(browser_httpRequests_getProjectsForBrowser(res,true)).done(function(){
-    gameBrowser_allProjects = res.data //copy http response to gameBrowser_allProjects
-    console.log(gameBrowser_allProjects)
-  })
+    //pass by reference in js only works on contents of objs
+    var res = {
+        "data" : {} //stores the results of the http request temporarily
+    };
+    +  $.when(browser_httpRequests_getProjectsForBrowser(res,true)).done(function(){
+        gameBrowser_allProjects = res.data; //copy http response to gameBrowser_allProjects
+        console.log(gameBrowser_allProjects);
+        if(gameBrowser_allProjects != {})
+            for(var p = 0; p<gameBrowser_allProjects.length; p++)
+                gameBrowser_displayGame(gameBrowser_allProjects[p], p);
+        else
+            $('#main-content').innerHTML = "<h1>There are no games to show</h1>";
+
+        /*
+        //old way of retrieving projects
+         for(var u = 0; u < gameBrowser_allProjects.users.length; u++){
+            for (var p = 0; p < gameBrowser_allProjects.users[u].projects.length; p++){
+                gameBrowser_displayGame(gameBrowser_allProjects.users[u].projects[p]);
+                console.log(gameBrowser_allProjects.users[u].projects[p]);
+
+            }//iterate through user's projects
+        } //iterate through users
+         */
+        $('.loading-content').hide();
+        $('#main-content').show();
+    })
 
 }
 
-function gameBrowser_addGame(gameObj){
+function gameBrowser_displayGame(gameObj, projNumber){
 
-    var gameTitle;
-    var gameAuthor;
-    var gameDescription;
-    var gameImage;
-    var gameLink;
 
-var gameCardHtml = '<div class="col s12 m4 l3"> '
-                   +     '<div class="card medium"> <div class="card-image">'
-                   +             ' <img src="http://materializecss.com/images/sample-1.jpg">'
-                   +                 '<span class="card-title">Game Title Goes Here <br/><span style="font-size: 18px;"> Author: Jon Doe</span></span>'
-                   +             '</div>'
-                   +             '<div class="card-content"> '
-                   +                 '<p>Game Description Will Go Here</p> '
-                   +             '</div> '
-                   +             '<div class="card-action">'
-                   +                 ' <a href="#">Play Game</a>'
-                   +             ' </div> '
-                   +     '</div> '
-                   + '</div>';
+    var gameCardHtml = '<div class="col s12 m6 l3">'
+        +     '<div class="card medium z-depth-3 darken-2"> <div class="card-image" id="project-' + projNumber + '-image">'
+        //+             ' <img src="' + gameObj.imageLink + '">'
+        +                 '<span class="card-title"><p>' + gameObj.title + '</p><p style="font-size: 18px;">Created By: '+ gameObj.author +'</p></span>'
+        +             '</div>'
+        +             '<div class="card-content white-text"> '
+        +                 '<p>'+ gameObj.description +'</p> '
+        +             '</div> '
+        +             '<div class="card-action">'
+        +                 '<a href="'+ gameObj.gameLink +'"><span class="accent-color">Play Game</span></a>'
+        +             ' </div> '
+        +     '</div> '
+        + '</div>';
 
-    $('#games-list').append(gameCardHtml);
+    //Append to recommended list if we made the game
+    if(gameObj.author == "Admin")
+        $('#recommended-games-list').append(gameCardHtml);
+    else
+        $('#all-games-list').append(gameCardHtml);
+
+    var displayImg = $('#project-'+ projNumber +'-image');
+    displayImg.css('background-image', 'url(' + gameObj.imageLink + ')');
+    displayImg.css('background-size', 'cover');
+
 }
