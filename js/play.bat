bdfc5398f4e735e37d2a4f19ab8c98302609e29c
dpekkle <danielle.cerisier@gmail.com>
2016-10-18 16:55:00 +1100

    play.min.js sync

--- js/play.bat
+++ js/play.bat
@@ -1,6 +1,6 @@
 @echo on
-del ".\play.min.js"
+del ".\play\play.min.js"
 CALL .\compile.bat
 set files=.\main\dev.min.js .\play\show.js
-java -jar compiler.jar --js %files% --jscomp_off=uselessCode --js_output_file .\play.min.js
+java -jar compiler.jar --js %files% --jscomp_off=uselessCode --js_output_file .\play\play.min.js
 timeout 20
