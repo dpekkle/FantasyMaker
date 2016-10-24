90a8de9a655ad76d72e4c4a657761dc41a2ec731
dpekkle <danielle.cerisier@gmail.com>
2016-10-24 14:11:46 +1100

    Update Favicons. Removed unused code.

--- js/unused/element approach/compile.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-del ".\dev.min.js"
-java -jar compiler.jar --js *.js --jscomp_off=uselessCode --js_output_file dev.min.js
-PAUSE
--- js/unused/element approach/main.js
+++
@@ -1,7 +0,0 @@
-goog.require('dragdrog')
-goog.require('clickBehaviour')
-goog.require('saveload')
-console.log("Hello World");
-
-collapse(document.getElementById("page-elem"));
-collapse(document.getElementById("link-elem"));
